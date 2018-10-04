9090e7b01c0479019f9d1d0a39953a714164abb3
Patrik Palmér <patrik.palmer@gmail.com>
2018-10-04 06:40:23 +0200

    Fixes faulty git directory path
    Forgot to set '/' before '.git'...

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ const exec = util.promisify(require('child_process').exec);
 
 async function main() {
     try {
-        const changes = getChanges();
+        const changes = await getChanges();
         console.log(changes);
     } catch (err) {
         console.log(err);
+        process.exit(1);
     }
 }
 
 async function getChanges() {
     const opts = {replaceNewLine: true};
-    const start = await execute(`git --git-dir ${__dirname}.git describe --abbrev=0 --tags`, opts);
-    const next = await execute(`git --git-dir ${__dirname}.git describe --abbrev=0 $(git describe --abbrev=0)^`, opts);
-    return await execute(`git --git-dir ${__dirname}.git log --no-merges --pretty=format:' * %s' ${next}..${start}`);
+    const start = await execute(`git --git-dir ${__dirname}/.git describe --abbrev=0 --tags`, opts);
+    const next = await execute(`git --git-dir ${__dirname}/.git describe --abbrev=0 $(git describe --abbrev=0)^`, opts);
+    return await execute(`git --git-dir ${__dirname}/.git log --no-merges --pretty=format:' * %s' ${next}..${start}`);
 }
 
 async function execute(cmd, opts) {
