932678b77a8aef1e87f1469b647e2639a234650d
Patrik Palmér <patrik.palmer@gmail.com>
2018-10-04 07:24:01 +0200

    Adds support for supplying a local directory path
    Use dir key to specify a local git path ("node . --dir=some/git/path")

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+#!/usr/bin/env node
+'use strict';
+const path = require('path');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
-async function main() {
+async function main(dir) {
     try {
-        const changes = await getChanges();
+        const changes = await getChanges(dir);
         console.log(changes);
     } catch (err) {
         console.log(err);
@@ -11,11 +14,11 @@ async function main() {
     }
 }
 
-async function getChanges() {
+async function getChanges(dir) {
     const opts = {replaceNewLine: true};
-    const start = await execute(`git --git-dir ${__dirname}/.git describe --abbrev=0 --tags`, opts);
-    const next = await execute(`git --git-dir ${__dirname}/.git describe --abbrev=0 $(git describe --abbrev=0)^`, opts);
-    return await execute(`git --git-dir ${__dirname}/.git log --no-merges --pretty=format:' * %s' ${next}..${start}`);
+    const start = await execute(`git --git-dir ${dir} describe --abbrev=0 --tags`, opts);
+    const next = await execute(`git --git-dir ${dir} describe --abbrev=0 $(git --git-dir ${dir} describe --abbrev=0)^`, opts);
+    return await execute(`git --git-dir ${dir} log --no-merges --pretty=format:' * %s' ${next}..${start}`);
 }
 
 async function execute(cmd, opts) {
@@ -29,4 +32,15 @@ async function execute(cmd, opts) {
     return stdout;
 }
 
-main();
+const argv = require('yargs').argv
+let dir = argv.dir;
+if (dir) {
+    if (!dir.endsWith('.git')) dir += '/.git'
+    dir = path.resolve(dir);
+} else {
+    dir = process.cwd() + '/.git'
+}
+
+console.log(dir);
+
+main(dir);
